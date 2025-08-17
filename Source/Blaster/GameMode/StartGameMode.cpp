04a2581453f8460c459484b4b61f85ebd723f652
John Ayling <john.ayling@yahoo.com>
2025-08-17 16:51:58 -0500

    Add gamemode to detect graphics.

---
+++ Source/Blaster/GameMode/StartGameMode.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StartGameMode.h"
+#include "GameFramework/GameUserSettings.h"
+
+AStartGameMode::AStartGameMode()
+{
+    UGameUserSettings* GameUserSettings = UGameUserSettings::GetGameUserSettings();
+
+    if (GameUserSettings)
+    {
+		UE_LOG(LogTemp, Log, TEXT("GameUserSettings found."));
+        // Run the hardware benchmark
+        GameUserSettings->RunHardwareBenchmark(1, 1.0f, 1.0f);
+
+        // Apply the detected optimal settings
+        GameUserSettings->ApplyHardwareBenchmarkResults();
+
+        // Optionally, save the settings
+        GameUserSettings->SaveSettings();
+    }
+}
